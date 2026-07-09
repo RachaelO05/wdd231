c8b130d7428ed6a48e1a84871e10c136057048d9
RachaelO05 <romoore@byupathway.edu>
2026-07-09 17:28:48 +0200

    creating the cars in js

--- chamber/scripts/businesses.js
+++ chamber/scripts/businesses.js
@@ -1,5 +1,42 @@
-const url = 
+const url = "https://rachaelo05.github.io/wdd231/chamber/data/members.json";
+const cards = document.querySelector('#cards');
 
 async function getBusinessData() {
-    const response = await fetch(url)
+    const response = await fetch(url);
+    const data = await response.json();
+
+    displayBusinesses(data.members);
 }
+
+getBusinessData();
+
+const displayBusinesses = (businesses) => {
+    businesses.forEach((business) => {
+        let card = document.createElement('section');
+        let name = document.createElement('h2');
+        let description = document.createElement('p');
+        let address = document.createElement('p');
+        let phoneNumber = document.createElement('p');
+        let websiteURL = document.createElement('p');
+        let image = document.createElement('img');
+        let membershipLevel = document.createElement('p');
+
+        name.textContent = `${business.name}`;
+        description.textContent = `Description: ${business.description}`;
+        address.textContent = `Address: ${business.address}`;
+        phoneNumber.textContent = `Phone Number: ${business.phonenumber}`;
+        websiteURL.textContent = `Website: ${business.websiteurl}`;
+        image.textContent = `${business.image}`;
+        membershipLevel.textContent = `Membership Lever: ${business.membership}`;
+
+        card.appendChild(name);
+        card.appendChild(description);
+        card.appendChild(address);
+        card.appendChild(phoneNumber);
+        card.appendChild(websiteURL);
+        card.appendChild(image);
+        card.appendChild(membershipLevel);
+
+        cards.appendChild(card);
+    });
+};
