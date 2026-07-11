38ca226a656b5cbe65631acda028f3cb9bd03cd2
RachaelO05 <romoore@byupathway.edu>
2026-07-11 15:48:56 +0200

    chamber project

--- chamber/scripts/businesses.js
+++ chamber/scripts/businesses.js
@@ -1,20 +1,25 @@
 const url = "https://rachaelo05.github.io/wdd231/chamber/data/members.json";
 const cards = document.querySelector('#cards');
 
+const gridbtn = document.querySelector('#grid-btn');
+const listbtn = document.querySelector('#list-btn');
+
 async function getBusinessData() {
     const response = await fetch(url);
     const data = await response.json();
 
     displayBusinesses(data.members);
-}
+};
 
 getBusinessData();
+
+cards.classList.add('grid');
 
 const displayBusinesses = (businesses) => {
     businesses.forEach((business) => {
         let card = document.createElement('section');
+
         let name = document.createElement('h2');
-        let description = document.createElement('p');
         let address = document.createElement('p');
         let phoneNumber = document.createElement('p');
         let websiteURL = document.createElement('p');
@@ -22,24 +27,33 @@ const displayBusinesses = (businesses) => {
         let membershipLevel = document.createElement('p');
 
         name.textContent = `${business.name}`;
-        description.textContent = `Description: ${business.description}`;
-        address.textContent = `Address: ${business.address}`;
-        phoneNumber.textContent = `Phone Number: ${business.phonenumber}`;
-        websiteURL.textContent = `Website: ${business.websiteurl}`;
+
         image.setAttribute('src', business.image);
         image.setAttribute('alt', `${business.name} image`);
         image.setAttribute('loading', 'lazy');
         
-        membershipLevel.textContent = `Membership Level: ${business.membershiplevel}`;
+        address.innerHTML = `<span class="label"><strong>Address:</strong></span> ${business.address}`;
+        phoneNumber.innerHTML = `<span class="label"><strong>Phone Number:</strong></span> ${business.phonenumber}`;
+        websiteURL.innerHTML = `<span class="label"><strong>Website:</strong></span> ${business.websiteurl}`;
+        membershipLevel.innerHTML = `<span class="label"><strong>Membership Level:</strong></span> ${business.membershiplevel}`;
 
         card.appendChild(name);
-        card.appendChild(description);
+        card.appendChild(image);
         card.appendChild(address);
         card.appendChild(phoneNumber);
         card.appendChild(websiteURL);
-        card.appendChild(image)
         card.appendChild(membershipLevel);
 
         cards.appendChild(card);
     });
 };
+
+gridbtn.addEventListener('click', () => {
+    cards.classList.add('grid');
+    cards.classList.remove('list');
+});
+
+listbtn.addEventListener('click', () => {
+    cards.classList.add('list');
+    cards.classList.remove('grid');
+});
